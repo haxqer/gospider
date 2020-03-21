44e1d56bd71b9a820bc7307e2d4d2e6174ec59b0
haxqer <2845513252@qq.com>
2020-03-21 10:36:45 +0800

    mgtv: get channel list

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module git.trac.cn/nv/spider
 
 go 1.13
+
+require (
+	golang.org/x/net v0.0.0-20200320220750-118fecf932d8
+	golang.org/x/text v0.3.2
+)
--- main.go
+++ main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
 
 func main() {
-	fmt.Println(123)
+	resp, err := http.Get(
+		"https://list.mgtv.com/1/a1-a1--------c1-25---.html?channelId=1")
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error: status code", resp.StatusCode)
+		return
+	}
+
+	//e := determineEncoding(resp.Body)
+	//utf8Reader := transform.NewReader(resp.Body, e.NewDecoder())
+
+	all, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	printChannelList(all)
+	//
+	//fmt.Printf("%s\n", all)
+
+}
+
+func determineEncoding(r io.Reader) encoding.Encoding {
+	bytes, err := bufio.NewReader(r).Peek(1024)
+	if err != nil {
+		panic(err)
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+
+	return e
+}
+
+func printChannelList(contents []byte) {
+	re := regexp.MustCompile(`<li><a[^>]*href="(/-------------\.html\?channelId=\d+)">([^<]+)</a></li>`)
+	matches := re.FindAllSubmatch(contents, -1)
+	for _, m := range matches {
+		fmt.Printf("Channel: %s,  URL: https://list.mgtv.com%s \n", m[2], m[1])
+	}
+
+	fmt.Printf("Matches found: %d\n", len(matches))
+
 }

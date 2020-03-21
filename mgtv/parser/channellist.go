20f7c7dee3e1f2971cb0ba27b472f1cbef3cd107
haxqer <2845513252@qq.com>
2020-03-21 18:15:50 +0800

    todo : next page

--- mgtv/parser/channellist.go
+++ mgtv/parser/channellist.go
@@ -7,18 +7,25 @@ import (
 
 var channelListRe = regexp.MustCompile(`<li><a[^>]*href="(/-------------\.html\?channelId=\d+)">([^<]+)</a></li>`)
 
+//var nextChannelPageRe = regexp.MustCompile(`<li><a href="([^>]+\.html\?channelId=\d+)" class="next turn" title="下一页">`)
+
 func ParseChannelList(contents []byte) engine.ParseResult {
 	matches := channelListRe.FindAllSubmatch(contents, -1)
 
 	result := engine.ParseResult{}
 	for _, m := range matches {
-		result.Items = append(result.Items, "Channel "+string(m[2]))
+		result.Items = append(result.Items, "Channel "+string(m[2])+" page 1")
 		result.Requests = append(result.Requests, engine.Request{
 			Url:        "https://list.mgtv.com" + string(m[1]),
 			ParserFunc: ParseChannel,
 		})
 
 	}
+	//
+	//nextChannelPageMatches := nextChannelPageRe.FindSubmatch(contents)
+	//if len(nextChannelPageMatches) > 2 {
+	//	result.Items = append(result.Items, "Channel "+string(m[2]))
+	//}
 
 	return result
 }
